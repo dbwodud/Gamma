d654f6642399c3bdb82c336514157abeddf15b16
dbwodud <dbwodud34@gmail.com>
2018-04-15 16:41:41 +0900

    In Linux

---
+++ CMakeFiles/Gamma.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Gamma.dir/Gamma.cpp.o"
+  "CMakeFiles/Gamma.dir/lexer.cpp.o"
+  "CMakeFiles/Gamma.dir/LLparser.cpp.o"
+  "CMakeFiles/Gamma.dir/Ast.cpp.o"
+  "Gamma.pdb"
+  "Gamma"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Gamma.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
