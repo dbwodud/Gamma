d19aa9c46819bbea37311a2c133a65ba22c8d5dd
dbwodud <dbwodud34@gmail.com>
2018-11-02 19:51:09 +0900

    add JIT

--- Gamma.cpp
+++ Gamma.cpp
@@ -1,10 +1,4 @@
 #include "Gamma.h"
-// JIT File ============================
-//#include "llvm/Support/TargetSelect.h"
-//#include "llvm/Target/TargetMachine.h"
-//#include "KaleidoscopeJIT.h"
-//======================================
-//static std::unique_ptr<KaleidoscopeJIT> TheJIT;
 symbol_table Symbol_table;
 
 int main(int argc,char *argv[]) {
@@ -15,16 +9,18 @@ int main(int argc,char *argv[]) {
     }
 
     FILE *fp = fopen(argv[1], "r");
+
     
     lexer(fp);
     printTokens();
-    //fclose(fp);
+
+
+    llvm::InitializeNativeTarget();
+    llvm::InitializeNativeTargetAsmPrinter();
+    llvm::InitializeNativeTargetAsmParser();
+
     TheModule=llvm::make_unique<llvm::Module>("My Cool jit",TheContext);
     Driver();
 
-    //llvm::InitializeNativeTarget();
-    //InitializeNativeTargetAsmPrinter();
-    //InitializeNativeTargetAsmParser();
-    //TheJIT = std::make_unique<KaleidoscopeJIT>();
     return 0;
 }
